55c8c4be9744f48edf8c9a8526084b58d917ca31
Mykyta Podoltsev <den00571@gmail.com>
2026-06-16 04:17:39 +0100

    Create interactive CLI menu

--- main.py
+++ main.py
@@ -24,3 +24,39 @@ def create_character():
 
     return Character(health, name, attack)
 
+print("Welcome to RPG Manager v0.1\n")
+print("Create your character!\n")
+main_character = create_character()
+dummy = Character(100, "Dummy", 10)
+
+while True:
+    print("""
+          1. Show Status
+          2. Fight Dummy
+          3. Exit
+          """)
+    
+    choice = input("Choose your action: \n").lower().strip()
+
+    if choice in ["1", "showstatus"]:
+        main_character.show_status()
+    elif choice in ["2", "fightdummy"]:
+        while main_character.health > 0 and dummy.health > 0:
+            main_character.attack_sequence(dummy)
+
+            if not dummy.health_check():
+                break
+
+            dummy.attack_sequence(main_character)
+
+            if not main_character.health_check():
+                break
+        main_character.heal_full()
+        dummy.heal_full()
+        print("Character rested up, HP restored to max.")
+    elif choice in ["3", "exit"]:
+        print("Thanks for playing! Self-annihilation...")
+        break
+    else:
+        print("Invalid command.")
+    
