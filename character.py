636e307a631baf31e746fd1ccbd680ac2dfaeabf
Mykyta Podoltsev <den00571@gmail.com>
2026-06-16 02:05:23 +0100

    Create HealthCheck method for Character class

--- character.py
+++ character.py
@@ -6,3 +6,9 @@ class Character:
     
     def ShowStatus(self):
         print(f"{self.Name} has {self.Health} HP and {self.Attack} ATK.\n")
+
+    def HealthCheck(self):
+        self.ShowStatus()
+        if (self.Health <= 0):
+            print(f"{self.Name} has been defeated.\n")
+            quit()
