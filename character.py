cc0dd0c04c48a86191330b9caaeb01a731f2e240
Mykyta Podoltsev <den00571@gmail.com>
2026-06-16 03:47:22 +0100

    Swap quit() for return in health check, move show_status to take_damage method

--- character.py
+++ character.py
@@ -8,15 +8,17 @@ class Character:
         print(f"{self.name} has {self.health} HP and {self.attack} ATK.\n")
 
     def health_check(self):
-        self.show_status()
         if (self.health <= 0):
             print(f"{self.name} has been defeated.\n")
-            quit()
+            return False
+        else: 
+            return True
 
     def take_damage(self, attacker):
         self.health = self.health - attacker.attack
         print(f"{self.name} took {attacker.attack} damage from {attacker.name}.\n")
         self.health_check()
+        self.show_status()
 
     def attack_sequence(self, target):
         print(f"{self.name} attacks {target.name}.\n")
