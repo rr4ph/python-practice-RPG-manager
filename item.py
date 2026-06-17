b37dab7773a8de598fe9734d39b9435bc138a162
Mykyta Podoltsev <den00571@gmail.com>
2026-06-17 03:57:51 +0100

    Add init for Item, add Potion and Sword template items

---
+++ item.py
@@ -0,0 +1,7 @@
+class Item:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+potion = Item("Potion", "desc1")
+sword = Item("Sword", "desc2")
